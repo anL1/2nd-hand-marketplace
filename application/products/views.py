4df5270cd86d1c0140fa1185a34d489c6bba32ba
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2019-03-26 15:13:11 +0200

    added missing import, url editing

--- application/products/views.py
+++ application/products/views.py
@@ -1,6 +1,6 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from application.products.models import Product
 from application.products.forms import ProductForm
@@ -22,22 +22,22 @@ def product_ad_create():
         return render_template("products/new_ad.html", form = form)
 
     p = Product(form.name.data, form.price.data)
-    p.account_id = current_user.id 
+    p.account_id = current_user.id
     db.session().add(p)
     db.session().commit()
 
     return redirect(url_for("products_index"))
 
-@app.route("/products/<product_id>/", methods=["GET"])
+@app.route("/products/<product_id>/edit/", methods=["GET"])
 def product_ad_page(product_id):
-    return render_template("products/product_ad.html", product = Product.query.get(product_id), form = ProductForm())
+    return render_template("products/edit_ad.html", product = Product.query.get(product_id), form = ProductForm())
 
-@app.route("/products/<product_id>/", methods=["POST"])
+@app.route("/products/<product_id>/edit/", methods=["POST"])
 def product_ad_modify(product_id):
     product = Product.query.get(product_id)
     form = ProductForm(request.form)
     if not form.validate():
-        return render_template("products/product_ad.html", product = Product.query.get(product_id), form = form)
+        return render_template("products/edit_ad.html", product = Product.query.get(product_id), form = form)
 
     product.name = form.name.data
     product.price = form.price.data
