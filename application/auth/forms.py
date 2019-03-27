13029b037e0fb6d7785c37b6581d4aaff86ef32a
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2019-03-27 18:16:30 +0200

    registration form added, simple validations added to login-form

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
 
 class LoginForm(FlaskForm):
-    username = StringField("Käyttäjätunnus")
-    password = PasswordField("Salasana")
+    username = StringField("Käyttäjätunnus", [validators.InputRequired(), validators.Length(min=3)])
+    password = PasswordField("Salasana", [validators.InputRequired(), validators.Length(min=3)])
+
+    class Meta:
+        csrf = False
+
+class SignUpForm(FlaskForm):
+    name = StringField("Nimi", [validators.InputRequired(), validators.Length(min=3)])
+    username = StringField("Käyttäjätunnus", [validators.InputRequired(), validators.Length(min=3)])
+    password = PasswordField("Salasana", [validators.InputRequired(), validators.Length(min=3)])
 
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,11 +1,26 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, SignUpForm
 
 from application.products.models import Product
+
+@app.route("/auth/new/", methods=["GET", "POST"])
+def auth_create_new():
+    if request.method == "GET":
+        return render_template("auth/new.html", form = SignUpForm())
+
+    form = SignUpForm(request.form)
+    if not form.validate():
+        return render_template("auth/new.html", form = form)
+
+    u = User(form.name.data, form.username.data, form.password.data)
+    db.session().add(u)
+    db.session().commit()
+
+    return redirect(url_for("auth_login"))
 
 @app.route("/auth/login/", methods=["GET", "POST"])
 def auth_login():
